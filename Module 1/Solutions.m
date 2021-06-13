35fd740ac8deb6003889f8a84f91a4bdba7d49ad
Eser <eserrose@gmail.com>
2021-06-14 01:45:19 +0300

    Added solution for Q3

--- Module 1/Solutions.m
+++ Module 1/Solutions.m
@@ -49,3 +49,17 @@ axis equal
 legend('Three-body Motion')
 
 S_final_three = round([R(:,end);a(:,end)],5)
+
+%------------------------------------------%
+%-------------- Question 3 ----------------%
+%------------------------------------------%
+
+mu_star = 1.2150585e-2;
+l_star  = 3.850e5;       %km
+t_star  = 3.758e5;       %s
+v_star  = l_star/t_star; %km/s
+
+R0 = [-(7327.031+4677.975); -813.869; 0];
+V0 = ([1.137; -(10.237+0.013); 0] -  cross([0; 0; 1/t_star], R0)); %inertial to rotating
+S0_nodim = [R0/l_star; V0/v_star];
+tspan_nodim = tspan/t_star;
